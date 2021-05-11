32fdef9cde2845f724c14faaf8e17159cf8bbdd3
djack11 <djack11@hotmail.com>
2021-05-11 12:22:48 -0400

    first commit

---
+++ cobb6javacleanup/README.md
@@ -0,0 +1,3 @@
+# Cobb 6 Java Clean Up
+
+This repo holds the code from session 5/1/2021
---
+++ cobb6javacleanup/RelationalOperators.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    
+    // create variables
+    int a = 7, b = 11;
+
+    // value of a and b
+    System.out.println("a is " + a + " and b is " + b);
+
+    // == operator
+    System.out.println(a == b); 
+
+    // != operator
+    System.out.println(a != b); 
+
+    // > operator
+    System.out.println(a > b);  
+
+    // < operator
+    System.out.println(a < b);  
+
+    // >= operator
+    System.out.println(a >= b); 
+
+    // <= operator
+    System.out.println(a <= b);  
+  }
+}
